2a036f52d87e990f00d85ca9513de47e75ce449f
winarto <winarto_xyz@hotmail.com>
2020-03-28 16:55:16 +0700

    sesuaikan dgn config mesin local aye

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-app/';
+$config['base_url'] = 'http://localhost:99/ngobar-9-codeigniter/';
 
 /*
 |--------------------------------------------------------------------------
